a188f3277eaff65436326e5f13663a35ddb7bdb3
LikeBall <goodbye789@163.com>
2013-10-23 09:22:03 +0800

    xxx

--- Chat/src/com/haiqiang/chat/server/ChatServer.java
+++ Chat/src/com/haiqiang/chat/server/ChatServer.java
@@ -27,7 +27,7 @@ public class ChatServer {
 		
 		try {
 			while(true) {
-				ClientThread ct = new ClientThread(ss.accept());
+				ClientThread ct = new ClientThread(ss.accept(), sClient);
 				System.out.println("A Client connect!");
 				new Thread(ct).start();
 				sClient.add(ct);
@@ -41,62 +41,6 @@ public class ChatServer {
 				}
 			} catch (IOException e1) {
 				e1.printStackTrace();
-			}
-		}
-	}
-	
-	class ClientThread implements Runnable {
-		private Socket s = null;
-		private DataInputStream dis = null;
-		private DataOutputStream dos = null;
-		private boolean bConnected = false;
-		
-		ClientThread(Socket s) {
-			this.s = s;
-			try {
-				dis = new DataInputStream(this.s.getInputStream());
-				dos = new DataOutputStream(this.s.getOutputStream());
-				bConnected = true;
-			} catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		public void send(String str) {
-				try {
-					dos.writeUTF(str);
-					dos.flush();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		}
-		
-		public void run() {
-			try {
-				while(bConnected) {
-					String str = dis.readUTF();
-					System.out.println(str);
-					for(Iterator<ClientThread> cIte = sClient.iterator(); cIte.hasNext(); ) {
-						ClientThread ct = cIte.next();
-						ct.send(str);
-					}
-				}
-			} catch(EOFException e) {
-				System.out.println("Client closed!");
-				sClient.remove(s);
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if(s != null) {
-						s.close();
-					}
-					if(dis != null) {
-						dis.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
 			}
 		}
 	}

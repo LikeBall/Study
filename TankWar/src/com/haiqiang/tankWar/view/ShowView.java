80f9f260d343d8dfaeb8c831d29ca718802b0ffa
LikeBall <goodbye789@163.com>
2013-10-24 11:43:03 +0800

    all java source add git

--- TankWar/src/com/haiqiang/tankWar/view/ShowView.java
+++ TankWar/src/com/haiqiang/tankWar/view/ShowView.java
@@ -3,19 +3,27 @@ package com.haiqiang.tankWar.view;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-public class ShowView extends Frame{
 
-	@Override
-	public void paint(Graphics g) {
-//		super.paint(g);
-		g.fillOval(245, 475, 20, 20);
-	}
+import com.haiqiang.tankWar.param.Parameter;
+
+public class ShowView extends Frame{
+	
 	private static final long serialVersionUID = 1L;
+	
+	private int x = (Parameter.FRAME_WIDTH-20) / 2;
+	private int y = (Parameter.FRAME_HEIGHT - 30);
+	
+	public void paint(Graphics g) {
+		System.out.println("paint" + y);
+		g.fillOval(x, y--, 20, 20);
+	}
+	
 	public ShowView(String s) {
 		super(s);
 	}
+	
 	public void launch() {
-		this.setBounds(100, 100, 500, 500);
+		this.setBounds(100, 100, Parameter.FRAME_WIDTH, Parameter.FRAME_HEIGHT);
 		this.setResizable(false);
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
@@ -23,6 +31,15 @@ public class ShowView extends Frame{
 			}			
 		});
 		this.setVisible(true);
+		Thread t = new Thread(new PaintThread());
+		t.start();
 	}
 	
+	class PaintThread implements Runnable {
+		
+		public void run() {
+			System.out.println("run");
+			repaint(1);
+		}
+	}
 }
